3feafb3e2c2e345dcfb963c288726e2e72afeb87
Yohei <yoheih130725@gmail.com>
2024-05-26 13:46:43 +0900

    culculation method add

---
+++ MO_test9/MO_test9/Calculation.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MO_test9
+{
+    internal class Calculation
+    {
+        //転置行列を作る関数です。https://qiita.com/sekky0816/items/8c73a7ec32fd9b040127
+        double[,] Transpose(double[,] A) 
+        {
+
+            double[,] AT = new double[A.GetLength(1), A.GetLength(0)];
+
+            for (int i = 0; i < A.GetLength(1); i++)
+            {
+                for (int j = 0; j < A.GetLength(0); j++)
+                {
+                    AT[i, j] = A[j, i];
+                }
+            }
+            return AT;
+        }
+
+        //逆行列を作る関数です。
+        double[,] inverseMatrix(double[,] A)
+        {
+
+            int n = A.GetLength(0);
+            int m = A.GetLength(1);
+
+            double[,] invA = new double[n, m];
+
+            if (n == m)
+            {
+
+                int max;
+                double tmp;
+
+                for (int j = 0; j < n; j++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        invA[j, i] = (i == j) ? 1 : 0;
+                    }
+                }
+
+                for (int k = 0; k < n; k++)
+                {
+                    max = k;
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        if (Math.Abs(A[j, k]) > Math.Abs(A[max, k]))
+                        {
+                            max = j;
+                        }
+                    }
+
+                    if (max != k)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            // 入力行列側
+                            tmp = A[max, i];
+                            A[max, i] = A[k, i];
+                            A[k, i] = tmp;
+                            // 単位行列側
+                            tmp = invA[max, i];
+                            invA[max, i] = invA[k, i];
+                            invA[k, i] = tmp;
+                        }
+                    }
+
+                    tmp = A[k, k];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        A[k, i] /= tmp;
+                        invA[k, i] /= tmp;
+                    }
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (j != k)
+                        {
+                            tmp = A[j, k] / A[k, k];
+                            for (int i = 0; i < n; i++)
+                            {
+                                A[j, i] = A[j, i] - A[k, i] * tmp;
+                                invA[j, i] = invA[j, i] - invA[k, i] * tmp;
+                            }
+                        }
+                    }
+
+                }
+
+
+                //逆行列が計算できなかった時の措置
+                for (int j = 0; j < n; j++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (double.IsNaN(invA[j, i]))
+                        {
+                            Console.WriteLine("Error : Unable to compute inverse matrix");
+                            invA[j, i] = 0;//ここでは，とりあえずゼロに置き換えることにする
+                        }
+                    }
+                }
+
+
+                return invA;
+
+            }
+            else
+            {
+                Console.WriteLine("Error : It is not a square matrix");
+                return invA;
+            }
+
+        }
+
+        //行列どうしの積を計算する関数です。
+        double[,] MatrixTimesMatrix(double[,] A, double[,] B)
+        {
+
+            double[,] product = new double[A.GetLength(0), B.GetLength(1)];
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    for (int k = 0; k < A.GetLength(1); k++)
+                    {
+                        product[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+
+            return product;
+
+        }
+
+        //最小二乗法を疑似逆行列を用いて解決します。
+        double[,] approximation(double[,] A, double[,] B)
+        {
+
+            //Aの転置At, (A*At)の逆行列Ainを求めます。
+            double[,] At = Transpose(A);
+            double[,] Ain = inverseMatrix(MatrixTimesMatrix(At, A));
+
+            //Ain*At*B が答えです。
+
+            double[,] product = MatrixTimesMatrix(MatrixTimesMatrix(Ain, At), B);
+
+            return product;
+        }
+        
+
+
+
+
+
+
+    }
+}
